d80ecbdbfb031ffbf4f02648d70cebe1bc8858f8
Guz Alexander <kalimatas@gmail.com>
2012-03-04 15:59:24 +0400

    init screen

--- gameoflife.py
+++ gameoflife.py
@@ -5,8 +5,10 @@
 Conway's Game of life
 """
 
+import pygame, time, random
 from optparse import OptionParser
 from sprites.boxes import Box
+from pygame.locals import *
 
 VERSION = "%prog 1.0"
 DESCRIPTION = "Conway's Game of life"
@@ -17,8 +19,43 @@ DEFAULT_SIZE = 10
 CELL_DEAD = '.'
 CELL_LIVE = 'o'
 
-def main():
-    pass
+# colors
+black = (0, 0, 0)
+
+def init_screen(width, height, size):
+    """
+    Initialize pygame screen
+    """
+    pygame.init()
+    pygame.display.set_caption(DESCRIPTION)
+    screen = pygame.display.set_mode([width * size, height * size])
+    return screen
+
+def main(options, args):
+    # prepare boxes
+    boxes = [['' for i in range(options.width)] for j in range(options.height)]
+
+    # fill board with dead cells
+    board = [[CELL_DEAD for i in range(options.width)] for j in range(options.height)]
+
+    # set screen params
+    screen = init_screen(options.width, options.height, options.size)
+    clock = pygame.time.Clock()
+
+    running = False
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                exit()
+            if event.type == KEYDOWN and event.key == K_RETURN:
+                running = False
+
+        screen.fill(black)
+        clock.time(20)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     parser = OptionParser(version=VERSION, description=DESCRIPTION)
@@ -28,5 +65,5 @@ if __name__ == "__main__":
     parser.add_option("--debug", action="store_true", dest="debug", help="Debug flag", default=False)
 
     (options, args) = parser.parse_args()
-
-    main()
+    main(options, args)
+    pygame.quit()
